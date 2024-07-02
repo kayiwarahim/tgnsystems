5c1ae103abf4e7fd13fe4cf4dd99e8b0a769d9f7
kayiwarahim <kayiwarahim@gmail.com>
2024-07-02 08:37:11 +0300

    added a sweet alert message upon contact form submision

--- contactme.php
+++ contactme.php
@@ -4,6 +4,10 @@ require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+header('Content-Type: application/json');
+
+$response = array();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['fname'];
     $phone = $_POST['phone'];
@@ -24,25 +28,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Port = 587;
 
         // Recipients
-        $mail->setFrom('your_email@gmail.com', 'Mailer');
+        $mail->setFrom('your_email@gmail.com', 'TGN SYTEMS');
         //$mail->addAddress('kayiwarahim@gmail.com', 'Kayiwa Rahim'); // Add the first recipient
         $mail->addAddress('kayiwa.rahim@tgnsystems.org', 'Kayiwa Rahim'); // Add the second recipient
         $mail->addAddress('admin@tgnsystems.org', 'Admin'); // Add the third recipient
+        
 
         // Content
         $mail->isHTML(true);
-        $mail->Subject = 'New Contact Form Submission';
-        $mail->Body    = "Name: $name<br>Contact Number: $phone<br>Email: $email<br>Message: $message";
-        $mail->AltBody = "Name: $name\nContact Number: $phone\nEmail: $email\nMessage: $message";
+        $mail->Subject = 'Clients contact submission';
+        $mail->Body    = "Name          : $name<br>
+                          Contact Number: $phone<br>
+                          Email         : $email<br>
+                          Message       : $message";
+        //$mail->AltBody = "Name: $name\nContact Number: $phone\nEmail: $email\nMessage: $message";
 
         $mail->send();
-        echo 'Message has been sent';
+        $response['status'] = 'success';
+        $response['message'] = 'Message has been sent successfully!';
+       // echo 'Message has been sent';
 
         // Redirect to the home page after 2 seconds
-        header("Refresh: 0; URL=index.html#contact"); // Change 'index.php' to your actual home page URL
-        exit();
+        //header("Refresh: 0; URL=index.html#contact"); // Change 'index.php' to your actual home page URL
+        //exit();
     } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        $response['status'] = 'error';
+        $response['message'] = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
+    echo json_encode($response);
+    exit();
+
 }
 ?>
